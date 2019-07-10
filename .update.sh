239d42baf7245cd00e0d7417134c5de52c72dd2f
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2019-07-10 18:20:59 +0700

    Update .update.sh

--- .update.sh
+++ .update.sh
@@ -1,4 +1,4 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/data/data/com.termux/files/usr/bin/sh
 
 check_package() {
     if [ "$(dpkg-query -W -f='${Status}' "$1" 2>/dev/null | grep -c 'ok installed')" -eq 0 ];
